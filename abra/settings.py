1069c3f144193363fb2bde9926819113f2552315
Roy Melamed <melamed12321@gmail.com>
2022-06-03 21:31:51 +0300

    Added django-heroku

--- abra/settings.py
+++ abra/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import django_heroku
 
 
 
@@ -161,3 +162,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
