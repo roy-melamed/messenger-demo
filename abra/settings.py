79a3686733b673276828b8798782a49d463a2b7a
Roy Melamed <melamed12321@gmail.com>
2022-06-03 20:07:44 +0300

    Updated STATIC_ROOT

--- abra/settings.py
+++ abra/settings.py
@@ -12,11 +12,15 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-import django_heroku
+import os
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 
 # Quick-start development settings - unsuitable for production
