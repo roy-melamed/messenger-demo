76ea1c9731fc881707bd7bff2f19294a569d24b1
Roy Melamed <melamed12321@gmail.com>
2022-06-04 19:24:47 +0300

    updated allowed hosts

--- abra/settings.py
+++ abra/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['roy-melamed-messenger.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['roy-messenger.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
