cd3dd8e7949fccf82d6fc7b8e61854ad50cfbb47
Roy Melamed <melamed12321@gmail.com>
2022-06-05 19:46:53 +0300

    fix admin url

--- abra/urls.py
+++ abra/urls.py
@@ -8,8 +8,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('messenger_api.urls')),
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('/api-auth/', include('rest_framework.urls')),
+    path('/api/', include('messenger_api.urls')),
+    path('/api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('/api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
