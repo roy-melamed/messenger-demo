8653072ba2e81cf0039db09b4ac85b27e1044ed6
Roy Melamed <melamed12321@gmail.com>
2022-06-05 14:47:44 +0300

    testing

--- abra/settings.py
+++ abra/settings.py
@@ -124,7 +124,7 @@ REST_FRAMEWORK = {
     #         'rest_framework.permissions.IsAuthenticated',
     #     ]
     'DEFAULT_PERMISSION_CLASSES': (
-            'rest_framework.permissions.AllowAny',
+            'rest_framework.permissions.IsAuthenticated',
         ),
 }
 
